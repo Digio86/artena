a60d4216f914856159bce4274a90596da6db59ec
Digio86 <digiorgio.mattia@gmail.com>
2017-03-16 14:36:11 +0100

    Aggiornamento Artena 16032017 - 14:36

---
+++ assets/javascript/custom/policy.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000",
+    },
+    "button": {
+      "background": "#cfb53b"
+    }
+  },
+  "content": {
+    "message": "Questo sito utilizza cookie, anche di terze parti, per inviarti servizi in linea con le tue preferenze. Se vuoi saperne di più o negare il consenso a tutti o ad alcuni cookie leggi l'informativa estesa sui cookie.",
+    "dismiss": "Accetta",
+    "link": "Leggi...",
+    "href": "http://www.artena.eu/test/cookie-policy/"
+  }
+})});
